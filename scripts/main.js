2ffe16f6b86e605f39b71fbddbd8b3de9d71eb2d
surovegor <surovpro@gmail.com>
2025-09-05 21:04:13 +0400

    feat: add header component

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const menuButton = document.querySelector('.header__menu-button'),
+      dropdownMenu = document.querySelector('.header__menu-dropdown');
+
+menuButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('header__menu-dropdown_show');
+});
+
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.header__menu-item')) {
+    dropdownMenu.classList.remove('header__menu-dropdown_show');
+  }
+});
+
+const burgerButton = document.querySelector('.header__burger-button'),
+      header = document.querySelector('.header');
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('active');
+  header.classList.toggle('menu-opened');
+});
